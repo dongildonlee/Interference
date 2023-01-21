17d93907f5d0bc0ad11e8dac5e904e5de95e10dc
Dongil Lee <dongil.don.lee@gmail.com>
2023-01-21 12:36:29 +0900

    add comment to a function

--- codes/packages/load_csv.py
+++ codes/packages/load_csv.py
@@ -1,5 +1,15 @@
 import pandas as pd
 
 def units_for_svm(path, num_units, net, epoch, relu):
+     ###########################################################
+    ## INPUT:
+    #   1. path: path to the csv file
+    #   2. num_units: number of units used in SVM
+    #   3. net: network ID #
+    #   4. epoch: training epoch #
+    #   5. relu: relu layer #
+    ## OUTPUT:
+    #   an array of unit IDs
+    ###########################################################
     uoi = pd.read_csv(path+'/'+str(num_units)+' randomly sampled units from distribution of both units in He untrained net'+str(net)+' epoch'+str(epoch)+ ' relu'+str(relu)+'.csv').drop(columns=['Unnamed: 0'])['0'].to_numpy()
     return uoi
